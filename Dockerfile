105326a857bca64831acede8a6f46f01c3a37d92
sijibomi <gbemilesanmi@gmail.com>
2023-11-25 00:51:23 +0100

    docker suff

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,10 @@
 FROM oven/bun
-
 WORKDIR /app
-
 COPY package.json .
 COPY bun.lockb .
-
 RUN bun install
-
 COPY src src
 COPY tsconfig.json .
-
 ENV NODE_ENV development 
-CMD ["bun", "src/index.ts"]
-
+CMD ["bun", "dev"]
 EXPOSE 3000
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia().get("/", () => "Hello Elysaa and everyone").listen(3000);
 
 console.log(
   `Server is running at ${app.server?.hostname}:${app.server?.port}`
