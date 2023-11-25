c9ae0f3c1e9ccd5ac2122d4fe57a552830e09bc1
sijibomi <gbemilesanmi@gmail.com>
2023-11-25 01:58:43 +0100

    redis db install

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,34 @@
 import { Elysia } from "elysia";
+import Redis from "ioredis"
 
 const app = new Elysia()
+export const redis = new Redis({
+    port: 6379,
+    host:"redis"
+})
+
+//INFO: to test if redis is working
+const redisTest = async () => {
+    console.log("Testing beginning")
+    try {
+        await redis.set("mykey", "value is the key i inserted at the beginning of the code");
+        redis.get("mykey", (err, result) => {
+            if (err) {
+                console.error(err);
+            } else {
+                console.log(result); // Prints "value"
+            }
+        });
+    } catch (e) {
+        console.error(e)
+    }
+    console.log("Testing end")
+}
+
+await redisTest()
+
+//INFO: test ended!
+
 app.get("/", () => "Welcome to this blog server bitch boy hhhhhhhhhh")
 app.listen(3000, () => {
     console.log(
